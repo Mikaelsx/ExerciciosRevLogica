d16dc65c7764c9a0b650b44a9c1300788cc06f7d
Mikael Souza Da França Falcão <51915337801@info.local>
2024-08-09 16:56:29 -0300

    09/08

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Exercício 1
+
+// Escreva um programa que peça ao usuário para digitar um número inteiro e informe se o
+// número é par ou ímpar. Utilize uma estrutura condicional if/else para realizar o teste.
+int numero = 0;
+int impar = numero-=1;
+
+Console.WriteLine($"Digite um numero:");
+numero = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"---------------------");
+Console.WriteLine($"O Numero {numero} é:");
+
+if (numero != impar)
+{
+Console.WriteLine($"O numero é Par!");
+}
+else
+{
+Console.WriteLine($"O numero é Impar!");
+}
+
+// Exercício 2
+
+// Crie um programa que permita que o usuário cadastre 5 nomes em um vetor. Após o
+// cadastro, o programa deve imprimir na tela os nomes cadastrados em ordem alfabética.
+// Utilize um laço for para o cadastro e um método de ordenação de sua preferência (por
+// exemplo, bubble sort) para ordenar os nomes.
